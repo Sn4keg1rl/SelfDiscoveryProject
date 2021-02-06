e26921aee631d16d71e54c4ac9a11c8c3673efc7
Walter Marino <wax94@hotmail.it>
2021-02-06 17:42:37 +0100

    Aggiunto Hover di Base

--- SelfDiscoveryProject/Assets/Script/Player.cs
+++ SelfDiscoveryProject/Assets/Script/Player.cs
@@ -9,9 +9,14 @@ public class Player : MonoBehaviour
     public float gravity;
     public float speed;
     private float appliedSpeed;
-    
+
+    public float nextJumpDelay;
+    private float currentNextJumpDelay;
+    public float jumpHeight;
     private bool isGrounded;
-    private bool roofReached;
+    public Transform feetPosition;
+    public float checkRadius;
+    public LayerMask groundType;
 
     Vector3 velocity;
 
@@ -25,10 +30,29 @@ public class Player : MonoBehaviour
     void Update()
     {
         Movement();
+        Glide();
     }
 
+    private void Glide()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            if (Time.time > currentNextJumpDelay)
+            {
+                velocity.y = Mathf.Sqrt(jumpHeight * -2 * gravity);
+                currentNextJumpDelay = Time.time + nextJumpDelay;
+            }
+        }
+        
+    }
     private void Movement()
     {
+        isGrounded = Physics.CheckSphere(feetPosition.position, checkRadius, groundType);
+
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = -1;
+        }
         float x = Input.GetAxis("Horizontal");
 
         Vector3 move = transform.right * x;
@@ -38,5 +62,11 @@ public class Player : MonoBehaviour
         velocity.y += gravity + Time.deltaTime;
 
         controller.Move(velocity * Time.deltaTime);
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(feetPosition.transform.position, checkRadius);
     }
 }
