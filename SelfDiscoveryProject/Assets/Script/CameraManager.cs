a86a0cdd2f4abcff9e42b8e9a1a11721a3bbffa8
Walter Marino <wax94@hotmail.it>
2021-02-06 16:41:16 +0100

    Aggiunto Movimento Base + Camera Manager

---
+++ SelfDiscoveryProject/Assets/Script/CameraManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraManager : MonoBehaviour
+{
+    public Transform target;
+
+    public float followSpeed;
+    public Vector3 offset;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 desideredPosition = target.position + offset;
+        Vector3 smoothedPosition = Vector3.Lerp(transform.position, desideredPosition, followSpeed);
+        transform.position = smoothedPosition;
+
+        transform.LookAt(target);
+    }
+}
---
+++ SelfDiscoveryProject/Assets/Script/Player.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private CharacterController controller;
+
+    public float gravity;
+    public float speed;
+    private float appliedSpeed;
+    
+    private bool isGrounded;
+    private bool roofReached;
+
+    Vector3 velocity;
+
+    private void Awake()
+    {
+        controller = GetComponent<CharacterController>();
+        appliedSpeed = speed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Movement();
+    }
+
+    private void Movement()
+    {
+        float x = Input.GetAxis("Horizontal");
+
+        Vector3 move = transform.right * x;
+
+        controller.Move(move * appliedSpeed * Time.deltaTime);
+
+        velocity.y += gravity + Time.deltaTime;
+
+        controller.Move(velocity * Time.deltaTime);
+    }
+}
